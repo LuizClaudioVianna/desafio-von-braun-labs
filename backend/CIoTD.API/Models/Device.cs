cc61b9691178b9b318c2355040ff9dfa0db454b6
Luiz Vianna <lcavianna@hotmail.com>
2025-05-03 17:47:38 -0300

    Adicionado o id como propriedade de busca na API

--- backend/CIoTD.API/Models/Device.cs
+++ backend/CIoTD.API/Models/Device.cs
@@ -2,6 +2,7 @@
 {
     public class Device
     {
+        public string Id { get; set; }
         public string Identifier { get; set; }
         public string Description { get; set; }
         public string Manufacturer { get; set; }
