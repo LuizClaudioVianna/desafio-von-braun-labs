88c1259a922f8ac9d578e3b17278e37a1d1325d5
Luiz Vianna <lcavianna@hotmail.com>
2025-05-03 23:10:49 -0300

    Incluindo tela de login e também fontaesome

--- frontend/ciotd/src/app/app.component.ts
+++ frontend/ciotd/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import {ButtonModule} from 'primeng/button'
 import { CardModule } from 'primeng/card';
 
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule,CardModule],
+  imports: [RouterOutlet, ButtonModule,CardModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/ciotd/src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CardModule, InputTextModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
