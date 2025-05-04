da643a8d73ad0f0f093e96bf4fa2c9d3681183d2
Luiz Vianna <lcavianna@hotmail.com>
2025-05-04 10:13:01 -0300

    Tela de login criada e estilizada

--- frontend/ciotd/src/app/pages/login/login.component.ts
+++ frontend/ciotd/src/app/pages/login/login.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule, InputTextModule],
+  imports: [CardModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  doLogin() {
+    throw new Error('Method not implemented.');
+  }
 
 }
