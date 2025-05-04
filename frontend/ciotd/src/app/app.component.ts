79c27f1c9866130cc55b6cfa1258bfbe727f6c76
Luiz Vianna <lcavianna@hotmail.com>
2025-05-03 21:00:27 -0300

    Adicionando primeng e primeflex no angular

--- frontend/ciotd/src/app/app.component.ts
+++ frontend/ciotd/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {ButtonModule} from 'primeng/button'
-
+import { CardModule } from 'primeng/card';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule,CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
