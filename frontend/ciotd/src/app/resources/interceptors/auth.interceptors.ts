259a28d7095a962ec1a1b9b1a9726ae542113d13
Luiz Vianna <lcavianna@hotmail.com>
2025-05-07 17:58:54 -0300

    Ajustes necessários e limpeza de código

--- frontend/ciotd/src/app/resources/interceptors/auth.interceptors.ts
+++ frontend/ciotd/src/app/resources/interceptors/auth.interceptors.ts
@@ -4,10 +4,7 @@ import { Observable } from "rxjs";
 export function authInterceptor(
     req: HttpRequest<unknown>,
     next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-    // console.log('authInterceptor');
-    // console.log('req', req);
-
-    //const headers = new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('access-token'));
+    
     const newReq = req.clone({
         headers: req.headers.set('Authorization','Bearer ' + localStorage.getItem('access-token')),
     });
